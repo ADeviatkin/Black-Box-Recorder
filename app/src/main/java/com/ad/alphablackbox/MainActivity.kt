bac224014538fa850c0ec1d6904878fb9b86ca4b
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-01-06 22:18:17 +0100

    working navigation bar

--- app/src/main/java/com/ad/alphablackbox/MainActivity.kt
+++ app/src/main/java/com/ad/alphablackbox/MainActivity.kt
@@ -5,23 +5,30 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.ad.alphablackbox.logic.Permissions.Companion.ensurePermissions
 import com.ad.alphablackbox.logic.ServiceBridge.Companion.connectToService
-import com.ad.alphablackbox.presentation.Toolbar
+import com.ad.alphablackbox.presentation.NavigationBar
 import android.view.View
+import com.ad.alphablackbox.logic.controll.SwipeListener
 
 class MainActivity : AppCompatActivity()
 {
-    private var toolbar = Toolbar()
+    private lateinit var navigation :NavigationBar
+    lateinit var swipelistener :SwipeListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_layout)
         ensurePermissions(this, applicationContext)
+        initiate()
         Log.d("App", "Try to connect")
         connectToService(applicationContext)
     }
+    private fun initiate()
+    {
+        navigation = NavigationBar(this)
+        swipelistener = SwipeListener(findViewById(R.id.main_layout), navigation)
+        navigation.setView(0)
+    }
     fun onClick(view :View)
     {
-        toolbar.switchView(view)
+        navigation.switchView(view)
     }
 }
-
---
+++ app/src/main/java/com/ad/alphablackbox/logic/controll/SwipeListener.java
@@ -0,0 +1,59 @@
+package com.ad.alphablackbox.logic.controll;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.ad.alphablackbox.presentation.NavigationBar;
+
+public class SwipeListener implements View.OnTouchListener
+{
+    GestureDetector detector;
+    int move_treshold = 100;
+    int velocity_treshold = 100;
+
+    // Constructor
+    public SwipeListener(View view, NavigationBar navigation)
+    {
+        GestureDetector.SimpleOnGestureListener listener = new GestureDetector.SimpleOnGestureListener()
+        {
+            @Override
+            public boolean onDown(MotionEvent e)
+            {
+                // screen touched
+                return true;
+            }
+
+            @Override
+            public boolean onFling(MotionEvent event1, MotionEvent event2, float velX, float velY)
+            {
+                // swipe
+                float xMove = event2.getX() - event1.getX();
+                float yMove = event2.getY() - event1.getY();
+                if (Math.abs(xMove) > Math.abs(yMove) && Math.abs(xMove) > move_treshold && Math.abs(velX) > velocity_treshold)
+                {
+                    // If it was horizontal swipe indeed
+                    if (xMove > 0) // right
+                    {
+                        navigation.switchLeft();
+                    }
+                    else // left
+                    {
+                        navigation.switchRight();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        };
+
+        detector = new GestureDetector(listener);
+    }
+
+    // Methods
+    @Override
+    public boolean onTouch(View view, MotionEvent me)
+    {
+        return detector.onTouchEvent(me);
+    }
+}
