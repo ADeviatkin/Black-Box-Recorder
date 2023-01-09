d4ee5c21a46ae2f22de0cdb9b579721425ec4be1
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-01-09 20:29:21 +0100

    Layouts rework + indicator bar

--- app/src/main/java/com/ad/alphablackbox/presentation/NavigationBar.kt
+++ app/src/main/java/com/ad/alphablackbox/presentation/NavigationBar.kt
@@ -1,5 +1,6 @@
 package com.ad.alphablackbox.presentation
 
+import android.graphics.Color
 import android.util.Log
 import android.view.View
 import com.ad.alphablackbox.MainActivity
@@ -10,15 +11,20 @@ class NavigationBar(del : MainActivity)
     private var currentView :Int = 0;
     private val layoutIds :IntArray = intArrayOf(R.layout.main_layout, R.layout.records_layout, R.layout.settings_layout)
     private val viewIds :IntArray = intArrayOf(R.id.main_layout, R.id.records_layout, R.id.settings_layout)
+    private val navBarIds :IntArray = intArrayOf(R.id.navigation_bar_indicator_1, R.id.navigation_bar_indicator_2, R.id.navigation_bar_indicator_3)
     private val delegate :MainActivity = del
 
     fun setView(c :Int)
     {
-        currentView = c
         delegate.setContentView(layoutIds[c])
         // listener
         var v :View = delegate.findViewById(viewIds[c])
         v.setOnTouchListener(delegate.swipelistener)
+        // setting new color
+        v = delegate.findViewById(navBarIds[c])
+        v.setBackgroundColor(Color.WHITE)
+
+        currentView = c
     }
     // public
     fun switchView(sender :View)
@@ -33,14 +39,16 @@ class NavigationBar(del : MainActivity)
     }
     fun switchRight()
     {
-        if (currentView < 2) currentView += 1
+        var c :Int = currentView
+        if (c < 2) c += 1
 
-        setView(currentView)
+        setView(c)
     }
     fun switchLeft()
     {
-        if (currentView > 0) currentView -= 1
+        var c :Int = currentView
+        if (c > 0) c -= 1
 
-        setView(currentView)
+        setView(c)
     }
 }
