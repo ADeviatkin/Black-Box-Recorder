205ab1c5ff16690547f9246a550087a1fce50739
CrazyPablo <71659677+CrazyPablo@users.noreply.github.com>
2023-01-10 20:46:11 +0100

    MainActivity update test code

--- app/src/main/java/com/ad/alphablackbox/MainActivity.kt
+++ app/src/main/java/com/ad/alphablackbox/MainActivity.kt
@@ -8,6 +8,11 @@ import android.view.View
 import com.ad.alphablackbox.logic.controll.OnClickManager
 import com.ad.alphablackbox.logic.controll.SwipeListener
 
+import com.ad.alphablackbox.logic.player.Player
+import com.ad.alphablackbox.logic.load.FilesLoader
+import android.os.Environment
+import android.util.Log
+
 class MainActivity : AppCompatActivity()
 {
     private lateinit var onclickmeneger : OnClickManager
@@ -24,6 +29,26 @@ class MainActivity : AppCompatActivity()
         onclickmeneger = OnClickManager(this)
         swipelistener = SwipeListener(findViewById(R.id.main_layout), onclickmeneger.navigation())
         onclickmeneger.setView(0)
+
+        val p=Player()
+        Log.d("App", "Media player created")
+
+        val path = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
+        if(path == null){
+            Log.d("App", "Can not access to Environment.DIRECTORY_DOWNLOADS")
+        }
+        else{
+            Log.d("App", "Path $path")
+            val fl = FilesLoader()
+            val files = fl.getAllFiles(path.toString())
+            var i=0
+            for (file in files) {
+                Log.d("App","$i : $file")
+                i++
+            }
+            p.play(files[1],applicationContext)
+            Log.d("App", "Song playing started")
+        }
     }
     fun onClick(button :View)
     {
